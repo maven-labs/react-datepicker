a7e4c73bd7f065f37da1a32bc852baa05a984555
Mark Rebec <mark@markrebec.com>
2020-09-02 18:51:13 -0700

    cleanup + gets typescript checking successfully

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+  /*
+  plugins: [
+    ["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }]
+  ],
+  "env": {
+    "test": {
+      "plugins": ["require-context-hook"]
+    }
+  },
+  */
+};
