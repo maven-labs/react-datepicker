01c115c31c24174f4f9e0ccc566d42d10d522560
Mark Rebec <mark@markrebec.com>
2020-09-03 14:07:09 -0700

    ditches legacy packages path

--- packages/styled/lib/testUtil.d.ts
+++
@@ -1,9 +0,0 @@
-declare function renderWithProviders(
-  ui: any,
-  options?: any,
-  contextValues?: Record<string, unknown>,
-): import('@testing-library/react').RenderResult<
-  typeof import('@testing-library/dom/types/queries')
->
-export * from '@testing-library/react'
-export {renderWithProviders as render}
